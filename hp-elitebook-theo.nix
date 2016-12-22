0707f360226def67fd227e3b5b7d7b5251a22a33
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2016-12-22 23:11:05 +0100

    Factorization (third step)

--- hp-elitebook-theo.nix
+++ hp-elitebook-theo.nix
@@ -5,66 +5,24 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
-    ];
+  imports = [
+    ./configuration-base.nix
+    ./qwerty.nix
+    ./laptops.nix
+  ];
 
   # Use the gummiboot efi boot loader.
   boot.loader = {
     gummiboot.enable = true;
     efi.canTouchEfiVariables = true;
-    timeout = 2;
   };
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  networking = {
-    hostName = "hp-elitebook-theo";
+  networking.hostName = "hp-elitebook-theo";
 
-    # I'm using network manager for easy eduroam support
-    networkmanager.enable = true;
-  };
-
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
-
-  # List packages installed in system profile.
-  # To search by name, use http://nixos.org/nixos/packages.html
   environment.systemPackages = with pkgs; [
-    nix-repl
-    wget
-    which
-    gnumake
-    zip
-    unzip
-    git
-    dmenu
-    i3lock
     xss-lock
-    networkmanagerapplet
     xorg.xbacklight
-    rxvt_unicode
-    irssi
-    rlwrap
-    emacs
-    firefox
-    libreoffice
-    evince
-    imagemagick
-    texlive.combined.scheme-full
   ];
-
-  # option does not exist !?
-  # nix.useSandbox = true;
-
-  programs.bash.enableCompletion = true;
 
   # List services that you want to enable:
 
@@ -73,22 +31,6 @@
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-
-    # Keyboard
-    layout = "us,us(intl)";
-    xkbOptions = "grp:alt_shift_toggle";
-
-    # Touchpad
-    # synaptics.enable = true;
-
-    displayManager.lightdm.enable = true;
-    desktopManager.xfce.enable = true;
-    windowManager.i3.enable = true;
-  };
 
   # Does it really work?
   services.logind.extraConfig = "HandleLidSwitch=suspend";
@@ -101,16 +43,4 @@
   '';
 
   fonts.fontconfig.dpi = 120;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.theo = {
-    isNormalUser = true;
-    home = "/home/theo";
-    description = "Théo Zimmermann";
-    extraGroups = [ "audio" ];
-  };
-
-  # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "16.09";
-
 }
