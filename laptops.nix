0583d6825fe2477e5b613093efe143ecdc8f6cb7
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2016-12-24 13:25:21 +0100

    Add laptop user to networkmanager group.

--- laptops.nix
+++ laptops.nix
@@ -9,4 +9,7 @@
 
   # Desktop Environment.
   services.xserver.desktopManager.xfce.enable = true;
+
+  # To allow normal-user theo to broadcast a wifi network
+  users.extraUsers.theo.extraGroups = [ "networkmanager" ];
 }
