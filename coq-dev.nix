e62c4fdb5dec45d53aa6f4874ddd058bcfdb5311
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-01-06 11:31:51 +0100

    Discriminate between stable and unstable packages in coq-dev environment.

--- coq-dev.nix
+++ coq-dev.nix
@@ -1,21 +1,23 @@
-with import <unstable> {};
+let
+  unstable = import <unstable> {};
+  stable = import <nixpkgs> {};
+in
 
-stdenv.mkDerivation {
+stable.stdenv.mkDerivation {
 
   name = "env";
 
-  buildInputs = [
-    ncurses
+  buildInputs = (with stable; [
+    stable.ncurses
 
     # Coq refman dependencies
     transfig
     ghostscript
     hevea
-    # These two are also required but I have them installed already
-    # texlive.combined.scheme-full
-    # imagemagick
+    texlive.combined.scheme-full
+    imagemagick
 
-  ] ++ (with ocamlPackages_4_02; [
+  ]) ++ (with unstable.ocamlPackages_4_02; [
     ocaml
     findlib
 
