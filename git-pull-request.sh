5b5bb44e25bc32a5725bb73149a423dfd38659f7
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-06-28 15:28:02 +0200

    Handle already checked out PRs as well.

--- git-pull-request.sh
+++ git-pull-request.sh
@@ -5,16 +5,21 @@
 set -e
 
 dir="`git rev-parse --show-toplevel`-pr-$1"
-remote=${2:-$(git remote | grep ^upstream || echo origin)}
 branch="pr/$1"
-ref="refs/pull/$1/merge"
 
-git fetch ${remote} ${ref}
-git branch ${branch} FETCH_HEAD
-git config branch.${branch}.remote ${remote}
-git config branch.${branch}.merge ${ref}
-git worktree add ${dir} ${branch}
-cd ${dir}
+if [[ "`git branch | grep ${branch}`" ]]; then
+  cd ${dir}
+  git pull --rebase
+else
+  remote=${2:-$(git remote | grep ^upstream || echo origin)}
+  ref="refs/pull/$1/merge"
+  git fetch ${remote} ${ref}
+  git branch ${branch} FETCH_HEAD
+  git config branch.${branch}.remote ${remote}
+  git config branch.${branch}.merge ${ref}
+  git worktree add ${dir} ${branch}
+  cd ${dir}
+fi
 bash # Let the user do their tests
 cd - > /dev/null
 read -p "Remove the worktree and the branch? [y/N] " resp
