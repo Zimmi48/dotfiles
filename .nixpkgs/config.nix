428eb3dd44a6f12839f848c8758e294d60e0590d
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-07-11 10:46:49 +0200

    Allow Skype install.

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,1 +1,5 @@
-{ packageOverrides = _ : { unstable = import <unstable> {}; }; }
+{ packageOverrides = _ : { unstable = import <unstable> {}; };
+  allowUnfreePredicate = with builtins; (pkg: elem (parseDrvName pkg.name).name [
+      "skype" # I never managed to make Skype work anyways
+    ]);
+}
