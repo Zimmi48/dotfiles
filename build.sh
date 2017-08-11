d5540c5e682293b8d0a5567bab9744a90b5a165e
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-08-12 01:19:47 +0200

    Update Coq to 8.6.1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-nix-build '<nixpkgs>' -A coq_8_6 -o nix-builds/coq-8-6
-nix-build '<nixpkgs>' -A coqPackages_8_6.mathcomp -o nix-builds/coq-8-6-mathcomp
+nix-build '<unstable>' -A coq_8_6 -o nix-builds/coq-8-6
+nix-build '<unstable>' -A coqPackages_8_6.mathcomp -o nix-builds/coq-8-6-mathcomp
 nix-build '<nixpkgs>' -A coq_8_5 -o nix-builds/coq-8-5
 nix-build '<nixpkgs>' -A coq_8_4 -o nix-builds/coq-8-4
 nix-build coq-dev.nix -o nix-builds/coq-dev
