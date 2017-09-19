8218411e025284f9015b5935f3fe1290d107f6bd
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-09-19 11:29:56 +0200

    Improve backporting script in case of failure.

--- git-backport.sh
+++ git-backport.sh
@@ -5,15 +5,37 @@
 set -e
 
 PRNUM=$1
-# Fails if PR does not exist:
-git log master --grep "Merge PR #${PRNUM}" | grep "." > /dev/null
+
+if ! git log master --grep "Merge PR #${PRNUM}" | grep "." > /dev/null; then
+    echo "PR #${PRNUM} does not exist."
+    exit 1
+fi
 
 BRANCH=backport-pr-${PRNUM}
 RANGE=$(git log master --grep "Merge PR #${PRNUM}" --format="%P" | sed 's/ /../')
 MESSAGE=$(git log master --grep "Merge PR #${PRNUM}" --format="%s" | sed 's/Merge/Backport/')
 
-git checkout -b ${BRANCH}
-git cherry-pick -x ${RANGE}
-git checkout -
+if git checkout -b ${BRANCH}; then
+
+    if ! git cherry-pick -x ${RANGE}; then
+        echo "Please fix the conflicts, then exit."
+        bash
+        while ! git cherry-pick --continue; do
+            echo "Please fix the conflicts, then exit."
+            bash
+        done
+    fi
+    git checkout -
+
+else
+
+    echo
+    read -p "Skip directly to merging phase? [y/N] " resp
+    if ! [[ "$resp" = "y" || "$resp" = "Y" ]]; then
+        exit
+    fi
+
+fi
+
 git merge -S ${BRANCH} --no-ff -m "${MESSAGE}"
 git branch -d ${BRANCH}
