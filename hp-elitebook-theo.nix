222b12e0e9a54b1324769474ece0ef3af1e9b49a
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-09-24 20:13:01 +0200

    Brightness keys are now detected without extra configuration.

--- hp-elitebook-theo.nix
+++ hp-elitebook-theo.nix
@@ -11,13 +11,6 @@
 
   boot.blacklistedKernelModules = [ "mei_wdt" ];
 
-  # Brightness up and down keys
-  services.udev.extraHwdb = ''
-    evdev:atkbd:dmi:*                 # all the built-in keyboards
-      KEYBOARD_KEY_97=brightnessup    # scan code: hexadecimal code minus 0x
-      KEYBOARD_KEY_92=brightnessdown  # (can be found with dmesg | tail)
-  '';
-
   services.xserver.libinput = {
     enable = true;
     naturalScrolling = true;
