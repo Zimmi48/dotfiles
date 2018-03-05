5645a66a40fdf9020d2db74e0a1eed2e4297dc20
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-03-05 20:30:14 +0100

    Upgrade. Disable math-comp for now because not using it.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 nix-build '<unstable>' -A coq_8_7 -o nix-builds/coq-8-7
-nix-build '<unstable>' -A coqPackages_8_7.mathcomp -o nix-builds/coq-8-7-mathcomp
+# nix-build '<unstable>' -A coqPackages_8_7.mathcomp -o nix-builds/coq-8-7-mathcomp
 nix-build '<nixpkgs>' -A coq_8_6 -o nix-builds/coq-8-6
 nix-build '<nixpkgs>' -A coq_8_5 -o nix-builds/coq-8-5
 nix-build '<nixpkgs>' -A coq_8_4 -o nix-builds/coq-8-4
