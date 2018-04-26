e92b24fafb888b3770b7d251b5c8cd785a24feae
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-04-26 23:37:28 +0200

    Use nix build instead of nix-build in ./build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-nix-build '<unstable>' -A coq_8_8 -o nix-builds/coq-8-8
-nix-build '<nixpkgs>' -A coq_8_7 -o nix-builds/coq-8-7
-nix-build '<nixpkgs>' -A coq_8_6 -o nix-builds/coq-8-6
-nix-build '<nixpkgs>' -A coq_8_5 -o nix-builds/coq-8-5
-nix-build '<nixpkgs>' -A coq_8_4 -o nix-builds/coq-8-4
-nix-build pykinea.nix -o nix-builds/pykinea
+nix build unstable.coq_8_8 -o nix-builds/coq-8-8
+nix build nixpkgs.coq_8_7 -o nix-builds/coq-8-7
+nix build nixpkgs.coq_8_6 -o nix-builds/coq-8-6
+nix build nixpkgs.coq_8_5 -o nix-builds/coq-8-5
+nix build nixpkgs.coq_8_4 -o nix-builds/coq-8-4
+nix build -f pykinea.nix -o nix-builds/pykinea
