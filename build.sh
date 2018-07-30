da75d0321460bc0ac729476fe64530e9f77cf58d
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-07-30 20:14:27 +0200

    Revert "Install mustache command-line."
    This reverts commit f096a0cd8f79d759f5b3ef6e4b83a27aa70f16f4.

--- build.sh
+++ build.sh
@@ -8,9 +8,8 @@ nix build nixpkgs.coq_8_6 -o nix-builds/coq-8-6
 nix build nixpkgs.coq_8_5 -o nix-builds/coq-8-5
 nix build nixpkgs.coq_8_4 -o nix-builds/coq-8-4
 nix build -f pykinea.nix -o nix-builds/pykinea
-nix build -f mustache.nix bin -o nix-builds/mustache
 
 nix-env -if https://github.com/cachix/cachix/tarball/master
 export CACHIX_SIGNING_KEY=$(pass tech/Cachix/theozim/CACHIX_SIGNING_KEY)
 cd nix-builds
-cachix push theozim pykinea mustache-bin
+cachix push theozim pykinea
