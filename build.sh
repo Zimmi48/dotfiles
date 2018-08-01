9e166fb3a4342cb62f38d97dfeabf4d4cb161a88
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-08-02 00:45:49 +0200

    Another, more generic, style.

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 nix build -f coq.nix coq-master -o nix-builds/coq-master
 nix build -f coq.nix coq-v8-8 -o nix-builds/coq-v8.8
-nix build unstable.coq_8_8 -o nix-builds/coq-8-8
-nix build nixpkgs.coq_8_7 -o nix-builds/coq-8-7
-nix build nixpkgs.coq_8_6 -o nix-builds/coq-8-6
-nix build nixpkgs.coq_8_5 -o nix-builds/coq-8-5
-nix build nixpkgs.coq_8_4 -o nix-builds/coq-8-4
+nix build -f '<unstable>' coq_8_8 -o nix-builds/coq-8-8
+nix build -f '<nixpkgs>' coq_8_7 -o nix-builds/coq-8-7
+nix build -f '<nixpkgs>' coq_8_6 -o nix-builds/coq-8-6
+nix build -f '<nixpkgs>' coq_8_5 -o nix-builds/coq-8-5
+nix build -f '<nixpkgs>' coq_8_4 -o nix-builds/coq-8-4
 nix build -f pykinea.nix -o nix-builds/pykinea
 
 nix-env -if https://github.com/cachix/cachix/tarball/master
