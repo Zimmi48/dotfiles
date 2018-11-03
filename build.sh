2d3e8991f51e5f5bab6655211f1b593769681db8
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-11-03 11:20:41 +0100

    Coq 8.4 is not in <nixpkgs> anymore.

--- build.sh
+++ build.sh
@@ -10,7 +10,6 @@ nix build -f nixpkgs coq_8_8 -o nix-builds/coq-8-8
 nix build -f nixpkgs coq_8_7 -o nix-builds/coq-8-7
 nix build -f nixpkgs coq_8_6 -o nix-builds/coq-8-6
 nix build -f nixpkgs coq_8_5 -o nix-builds/coq-8-5
-nix build -f nixpkgs coq_8_4 -o nix-builds/coq-8-4
 nix build -f pykinea.nix -o nix-builds/pykinea
 
 echo
