79de24168f795ed8376f0c53d761dbca304faa5b
Théo Zimmermann <theo.zimmermann@inria.fr>
2022-10-31 10:46:54 +0100

    Install VS Code extensions to work with Jupyter notebooks.

--- configuration-base.nix
+++ configuration-base.nix
@@ -128,7 +128,12 @@ in
           jnoortheen.nix-ide
           maximedenes.vscoq
           ms-python.python
+          ms-python.vscode-pylance
           ms-toolsai.jupyter
+          ms-toolsai.jupyter-keymap
+          ms-toolsai.jupyter-renderers
+          ms-toolsai.vscode-jupyter-cell-tags
+          ms-toolsai.vscode-jupyter-slideshow
           ms-vsliveshare.vsliveshare
           ocamllabs.ocaml-platform
         ]; })
