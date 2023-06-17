3e32fd97830a0db34c7152e1f4df8f47d2664238
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2023-06-17 12:43:13 +0200

    Improve tracking of NixOS configurations.

--- build.sh
+++ build.sh
@@ -1,29 +1,53 @@
 #!/bin/sh
 
-nix-build "$HOSTNAME.nix" -A system
+# Build the NixOS configuration for this machine.
+# Generate a unique result symlink filename containing the date and time.
+# This allows to keep roots of previous builds for a while.
+result="$HOSTNAME-$(date +%Y-%m-%d-%Hh%M)"
+echo "Building $HOSTNAME configuration..."
+nix-build "$HOSTNAME.nix" -A system -o "$result"
+echo "Result symlink: $result"
 
 # Old Coq releases
-nix-build nixpkgs -A coqPackages_8_16.coqide -o nix-builds/coq-8-16
-nix-build nixpkgs -A coqPackages_8_15.coqide -o nix-builds/coq-8-15
-nix-build nixpkgs -A coqPackages_8_14.coqide -o nix-builds/coq-8-14
-nix-build nixpkgs -A coq_8_13 -o nix-builds/coq-8-13
-nix-build nixpkgs -A coq_8_12 -o nix-builds/coq-8-12
-nix-build nixpkgs -A coq_8_11 -o nix-builds/coq-8-11
-nix-build nixpkgs -A coq_8_10 -o nix-builds/coq-8-10
-nix-build nixpkgs -A coq_8_9 -o nix-builds/coq-8-9
-nix-build nixpkgs -A coq_8_8 -o nix-builds/coq-8-8
-nix-build nixpkgs -A coq_8_7 -o nix-builds/coq-8-7
-nix-build nixpkgs -A coq_8_6 -o nix-builds/coq-8-6
+echo
+echo "Do you want to build old Coq releases? (y/n)"
+read -r answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+       echo "Building old Coq releases..."
+
+       nix-build nixpkgs -A coqPackages_8_16.coqide -o nix-builds/coq-8-16
+       nix-build nixpkgs -A coqPackages_8_15.coqide -o nix-builds/coq-8-15
+       nix-build nixpkgs -A coqPackages_8_14.coqide -o nix-builds/coq-8-14
+       nix-build nixpkgs -A coq_8_13 -o nix-builds/coq-8-13
+       nix-build nixpkgs -A coq_8_12 -o nix-builds/coq-8-12
+       nix-build nixpkgs -A coq_8_11 -o nix-builds/coq-8-11
+       nix-build nixpkgs -A coq_8_10 -o nix-builds/coq-8-10
+       nix-build nixpkgs -A coq_8_9 -o nix-builds/coq-8-9
+       nix-build nixpkgs -A coq_8_8 -o nix-builds/coq-8-8
+       nix-build nixpkgs -A coq_8_7 -o nix-builds/coq-8-7
+       nix-build nixpkgs -A coq_8_6 -o nix-builds/coq-8-6
+else
+       echo "Skipping old Coq releases..."
+fi
 
 # Dev version of Coq
-nix-build -E '(import ./nixpkgs {}).coq.override { version = "master"; buildIde = true; }' -o nix-builds/coq-master
+echo
+echo "Do you want to build the dev version of Coq? (y/n)"
+read -r answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+       echo "Building dev version of Coq..."
+
+       nix-build -E '(import ./nixpkgs {}).coq.override { version = "dev"; buildIde = true; }' -o nix-builds/coq-dev
+else
+       echo "Skipping dev version of Coq..."
+fi
 
 echo
 echo "Build completed."
 echo
 echo "Now run:"
 echo "pass -c tech/$(echo $HOSTNAME | cut -d- -f1-2)/rootpass
-su -c \"nix-env -p /nix/var/nix/profiles/system --set ./result &&
-       ./result/bin/switch-to-configuration switch\""
+su -c \"nix-env -p /nix/var/nix/profiles/system --set ./$result &&
+       ./$result/bin/switch-to-configuration switch\""
 
 # Reference: http://www.haskellforall.com/2018/08/nixos-in-production.html
