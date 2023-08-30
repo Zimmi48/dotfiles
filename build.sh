2924ca55d09973f3be2e5b28a25d383ffff06ff0
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2023-08-30 11:20:55 +0200

    Allow to skip tagging and committing.
    And always skip if there are no changes.
    Add explicit experimental feature flags for bootstrapping phase.

--- build.sh
+++ build.sh
@@ -12,11 +12,23 @@ else
        echo "Skipping the update of the input flakes..."
 fi
 
+# Are there any changes to commit?
+DIFF=$((git diff --exit-code > /dev/null 2>&1; echo $?) || echo $?)
+
 # Tagging the new configuration
-echo
-echo "What are the (space-separated) tags for the new configuration? (They will be reorded alphabetically.)"
-read -r -a tags
-echo "{ system.nixos.tags = [ $(printf '"%s" ' "${tags[@]}")]; }" > ./nixos-tags.nix
+# (silently skip if there are no changes to commit)
+if [ "$DIFF" -eq 1 ]; then
+       echo
+       echo "Do you want to tag the new configuration? (Y/n)"
+       read -r answer
+       if [ "$answer" != "${answer#[Nn]}" ]; then
+              echo "Skipping the tagging of the new configuration..."
+       else
+              echo "What are the (space-separated) tags for the new configuration? (They will be reorded alphabetically.)"
+              read -r -a tags
+              echo "{ system.nixos.tags = [ $(printf '"%s" ' "${tags[@]}")]; }" > ./nixos-tags.nix
+       fi
+fi
 
 # Build the NixOS configuration for this machine.
 # Generate a unique result symlink filename containing the date and time.
@@ -24,11 +36,11 @@ echo "{ system.nixos.tags = [ $(printf '"%s" ' "${tags[@]}")]; }" > ./nixos-tags
 result="$HOSTNAME-$(date +%Y-%m-%d-%Hh%M)"
 echo
 echo "Building $HOSTNAME configuration..."
-nix build .#nixosConfigurations."$HOSTNAME".config.system.build.toplevel --no-warn-dirty --out-link ./nix-builds/$result
+nix build .#nixosConfigurations."$HOSTNAME".config.system.build.toplevel --no-warn-dirty --out-link ./nix-builds/$result --experimental-features 'nix-command flakes'
 echo "Build completed."
 echo
 echo "Closure differences:"
-nix store diff-closures /nix/var/nix/profiles/system ./nix-builds/$result
+nix store diff-closures /nix/var/nix/profiles/system ./nix-builds/$result --experimental-features 'nix-command flakes'
 echo
 echo "Result symlink: nix-builds/$result"
 
@@ -40,16 +52,22 @@ if [ "$answer" = "t" ] || [ "$answer" = "T" ]; then
        echo "Testing the new configuration..."
        pass -c tech/$(echo $HOSTNAME | cut -d- -f1-2)/rootpass
        su -c "./nix-builds/$result/bin/switch-to-configuration test"
-       exit 0
 # If the user answer equals "S" or "s", then switch to the new configuration.
 elif [ "$answer" = "s" ] || [ "$answer" = "S" ]; then
-       echo "Committing the new configuration..."
-       git commit -am "$(cat ./nix-builds/$result/nixos-version)" -m "$(nix store diff-closures /nix/var/nix/profiles/system ./nix-builds/$result)" --edit -S
+       if [ "$DIFF" -eq 1 ]; then
+              echo "Do you want to commit the new configuration? (Y/n)"
+              read -r answer
+              if [ "$answer" != "${answer#[Nn]}" ]; then
+                     echo "Skipping the commit of the new configuration..."
+              else
+                     echo "Committing the new configuration..."
+                     git commit -am "$(cat ./nix-builds/$result/nixos-version)" -m "$(nix store diff-closures /nix/var/nix/profiles/system ./nix-builds/$result)" --edit -S
+              fi
+       fi
        echo "Switching to the new configuration..."
        pass -c tech/$(echo $HOSTNAME | cut -d- -f1-2)/rootpass
        su -c "nix-env -p /nix/var/nix/profiles/system --set ./nix-builds/$result &&
               ./nix-builds/$result/bin/switch-to-configuration switch"
-       exit 0
 fi
 
 # Reference: http://www.haskellforall.com/2018/08/nixos-in-production.html
